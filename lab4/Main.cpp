23fb9a44da99ea0731903c3923a5d1e3167fd4b3
necromein <123072314+necromein@users.noreply.github.com>
2026-05-03 21:19:49 +0300

    Add files via upload

---
+++ lab4/Main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include "labs.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "russian");
+
+    ofstream clearfile("lab4.txt");
+    clearfile.close();
+
+    lab1();
+    lab2();
+    lab3();
+
+    ifstream fin("lab4.txt");
+
+    string line;
+    while (getline(fin, line)) {
+        cout << line << endl;
+    }
+
+    fin.close();
+
+    return 0;
+}
